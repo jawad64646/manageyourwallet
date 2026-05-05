fd6efb49be3e339b83ea322009dd38591e83257e
Mohamad_ALsahily <mohamadalm119@gmail.com>
2026-05-06 01:41:10 +0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,14 @@
+/*
+Copyright 2026 Mohamad Alsahily
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+*/
+
+import 'package:flutter/material.dart';
 import 'package:flutter/material.dart';
 import 'package:pinput/pinput.dart';
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
